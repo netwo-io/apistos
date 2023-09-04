b4f9f15d1e1fff8524055c1cc31c64b9fa661c67
Romain Lebran <rlebran@netwo.com>
2023-09-04 14:56:03 +0200

    Re-expose TypedSchema

--- netwopenapi/src/lib.rs
+++ netwopenapi/src/lib.rs
@@ -9,5 +9,5 @@ pub mod web;
 
 pub use internal::components::error::ApiErrorComponent;
 pub use internal::components::parameters::header::ApiHeader;
-pub use internal::components::ApiComponent;
+pub use internal::components::{ApiComponent, TypedSchema};
 pub use netwopenapi_gen::{api_operation, ApiComponent, ApiCookie, ApiErrorComponent, ApiHeader, ApiSecurity};
