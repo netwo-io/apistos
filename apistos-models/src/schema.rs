7848911140233cb28b1c74c5deb8723deaf699d1
Romain Lebran <rlebran@gmail.com>
2024-06-10 18:09:41 +0200

    Use ApistosSchema instead of Schema

--- apistos-models/src/schema.rs
+++ apistos-models/src/schema.rs
@@ -1,5 +1,6 @@
 use crate::OpenApiVersion;
 use log::warn;
+use schemars::gen::SchemaSettings;
 use schemars::{Schema, SchemaGenerator};
 use serde::Serialize;
 use serde_json::Value;
@@ -31,6 +32,8 @@ impl ApistosSchema {
         Self::remove_definition_from_schema(obj, &oas_version.get_schema_settings().into_generator());
         match oas_version {
           OpenApiVersion::OAS3_0 => {
+            // remove definitions from schema
+            Self::remove_definition_from_schema(obj, &SchemaSettings::openapi3().into_generator());
             // remove $schema property
             obj.remove("$schema");
             Self(schemars::Schema::from(obj.clone()))
