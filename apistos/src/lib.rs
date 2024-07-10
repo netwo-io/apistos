e8c2bf3699cfc7d9a6090c28cb9abcd73f8cba8b
Romain Lebran <rlebran@gmail.com>
2024-07-10 11:48:13 +0200

    Add missing doc for actix-web-grants feature flag

--- apistos/src/lib.rs
+++ apistos/src/lib.rs
@@ -117,22 +117,23 @@
 //!
 //! # Feature flags
 //!
-//! | name           | description                                                                 | extra dependencies                                            |
-//! |----------------|-----------------------------------------------------------------------------|---------------------------------------------------------------|
-//! | `query` (default) | Enables documenting `actix_web::web::Query`                              |                                                               |
-//! | `actix` (default) | Enables documenting types from `actix`                                   |                                                               |
-//! | `lab_query`       | Enables documenting `actix_web_lab::extract::Query`                      | [`actix-web-lab`](https://crates.io/crates/actix-web-lab)     |
-//! | `garde`           | Enables input validation through `garde`                                 | [`garde`](https://crates.io/crates/garde)                     |
-//! | `qs_query`        | Enables documenting types from `serde_qs`                                | [`serde_qs`](https://crates.io/crates/serde-qs)               |
-//! | `rapidoc`         | Enables `RapiDoc` to expose the generated openapi file                   |                                                               |
-//! | `redoc`           | Enables `ReDoc` to expose the generated openapi file                     |                                                               |
-//! | `swagger-ui`      | Enables Swagger UI to expose the generated openapi file                  |                                                               |
-//! | `chrono`          | Enables documenting types from `chrono`                                  | [`chrono`](https://crates.io/crates/chrono)                   |
-//! | `multipart`       | Enables documenting types from `actix-multipart`                         | [`actix-multipart`](https://crates.io/crates/actix-multipart) |
-//! | `rust_decimal`    | Enables documenting types from `rust_decimal`                            | [`rust_decimal`](https://crates.io/crates/rust-decimal)       |
-//! | `uuid`            | Enables documenting types from `uuid`                                    | [`uuid`](https://crates.io/crates/uuid)                       |
-//! | `url`             | Enables documenting types from `url`                                     | [`url`](https://crates.io/crates/url)                         |
-//! | `extras`          | Enables `chrono`, `multipart`, `rust_decimal`, `uuid` and `url` features | All from previous features                                    |
+//! | name           | description                                                                 | extra dependencies                                             |
+//! |----------------|-----------------------------------------------------------------------------|----------------------------------------------------------------|
+//! | `query` (default) | Enables documenting `actix_web::web::Query`                              |                                                                |
+//! | `actix` (default) | Enables documenting types from `actix`                                   |                                                                |
+//! | `lab_query`       | Enables documenting `actix_web_lab::extract::Query`                      | [`actix-web-lab`](https://crates.io/crates/actix-web-lab)      |
+//! | `garde`           | Enables input validation through `garde`                                 | [`garde`](https://crates.io/crates/garde)                      |
+//! | `actix-web-grants`| Enables support for `actix-web-grants`                                   | [`actix-web-grants`](https://crates.io/crates/actix-web-grants)|
+//! | `qs_query`        | Enables documenting types from `serde_qs`                                | [`serde_qs`](https://crates.io/crates/serde-qs)                |
+//! | `rapidoc`         | Enables `RapiDoc` to expose the generated openapi file                   |                                                                |
+//! | `redoc`           | Enables `ReDoc` to expose the generated openapi file                     |                                                                |
+//! | `swagger-ui`      | Enables Swagger UI to expose the generated openapi file                  |                                                                |
+//! | `chrono`          | Enables documenting types from `chrono`                                  | [`chrono`](https://crates.io/crates/chrono)                    |
+//! | `multipart`       | Enables documenting types from `actix-multipart`                         | [`actix-multipart`](https://crates.io/crates/actix-multipart)  |
+//! | `rust_decimal`    | Enables documenting types from `rust_decimal`                            | [`rust_decimal`](https://crates.io/crates/rust-decimal)        |
+//! | `uuid`            | Enables documenting types from `uuid`                                    | [`uuid`](https://crates.io/crates/uuid)                        |
+//! | `url`             | Enables documenting types from `url`                                     | [`url`](https://crates.io/crates/url)                          |
+//! | `extras`          | Enables `chrono`, `multipart`, `rust_decimal`, `uuid` and `url` features | All from previous features                                     |
 //!
 //! It is possible to completely disable the documentation of `actix_web::web::Query`. This is useful when you want to enforce the use of `serde_qs::actix::QsQuery` in your project. To do so disable the default features. (Note: you might need to add `actix` feature as well)
 //!
@@ -149,9 +150,9 @@
 
 pub use indexmap::IndexMap;
 
+pub use apistos_core::{ApiComponent, ApiErrorComponent, TypedSchema};
 pub use apistos_core::parameters::header::ApiHeader;
 pub use apistos_core::PathItemDefinition;
-pub use apistos_core::{ApiComponent, ApiErrorComponent, TypedSchema};
 pub use apistos_gen::{api_operation, ApiComponent, ApiCookie, ApiErrorComponent, ApiHeader, ApiSecurity, ApiType};
 pub use apistos_models::*;
 #[cfg(feature = "rapidoc")]
