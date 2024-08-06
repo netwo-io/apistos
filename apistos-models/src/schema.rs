92464afc354ea37027b3616540c6c62e7a194d8c
Romain Lebran <rlebran@netwo.com>
2024-08-06 10:29:44 +0200

    Fix fmt

--- apistos-models/src/schema.rs
+++ apistos-models/src/schema.rs
@@ -104,12 +104,9 @@ impl ApistosSchema {
               sch_obj.entry("title").or_insert_with(|| prop_name.clone().into());
             }
           } else {
-            let const_values: Vec<&Value> = props.iter().filter_map(|(_, p)| {
-              p.as_object()
-                .and_then(|sch_obj| {
-                  sch_obj.get("const")
-                })
-            })
+            let const_values: Vec<&Value> = props
+              .iter()
+              .filter_map(|(_, p)| p.as_object().and_then(|sch_obj| sch_obj.get("const")))
               .collect();
             if const_values.len() == 1 {
               if let Some(Value::String(prop_name)) = const_values.first() {
