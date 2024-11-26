359e62104a8fd61dac853a6156d22aa93ecd441d
Romain Lebran <rlebran@gmail.com>
2024-11-26 08:40:32 +0100

    Fix example

--- examples/simple-todo-macros/src/api.rs
+++ examples/simple-todo-macros/src/api.rs
@@ -46,14 +46,5 @@ pub mod test {
         description: new_todo.description,
       }))
     }
-
-    #[get(path = "/custom_resource_name", name = "custom")]
-    async fn custom_resource_name_test<'a>(_body: Json<NewTodo>) -> Result<Json<Todo>, Error> {
-      Ok(Json(Todo {
-        id: Uuid::new_v4(),
-        title: "".to_string(),
-        description: None,
-      }))
-    }
   }
 }
