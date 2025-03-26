714b5ca29a82e6a8ec9cd2fc4d835a90a4ce212a
Olivier Kleparski <okleparski@netwo.com>
2025-03-26 14:25:02 +0100

    Test that both version works with configure

--- apistos/tests/service_config.rs
+++ apistos/tests/service_config.rs
@@ -1,15 +1,16 @@
 use actix_web::web::{Json, Path};
 use actix_web::{App, Error};
 
-use actix_web::test::{call_service, init_service, try_read_body_json, TestRequest};
+use actix_web::test::{TestRequest, call_service, init_service, try_read_body_json};
+use apistos::OpenApiVersion;
 use apistos::app::OpenApiWrapper;
 use apistos::spec::Spec;
-use apistos::web::{get, ServiceConfig};
+use apistos::web::{ServiceConfig, get};
 use apistos_gen::api_operation;
 use apistos_models::OpenApi;
 
 #[actix_web::test]
-async fn actix_nested_service_config_configure() {
+async fn actix_nested_service_config_configure_oas_3_1() {
   #[api_operation(tag = "pet")]
   pub(crate) async fn test(_params: Path<(u32, String)>) -> Result<Json<()>, Error> {
     Ok(Json(()))
@@ -24,7 +25,49 @@ async fn actix_nested_service_config_configure() {
   }
 
   let app = App::new()
-    .document(Spec::default())
+    .document(Spec {
+      openapi: OpenApiVersion::OAS3_1,
+      ..Default::default()
+    })
+    .configure(cfg_root)
+    .build("/openapi.json");
+  let app = init_service(app).await;
+
+  let req = TestRequest::get().uri("/openapi.json").to_request();
+  let resp = call_service(&app, req).await;
+  assert!(resp.status().is_success());
+
+  let body: OpenApi = try_read_body_json(resp).await.expect("Unable to read body");
+  let mut paths: Vec<&String> = body.paths.paths.keys().collect();
+  paths.sort();
+
+  let expected_paths = vec!["/"];
+
+  assert_eq!(paths, expected_paths);
+
+  assert_eq!(body.paths.paths.values().flat_map(|v| v.operations.values()).count(), 1);
+}
+
+#[actix_web::test]
+async fn actix_nested_service_config_configure_oas_3_0() {
+  #[api_operation(tag = "pet")]
+  pub(crate) async fn test(_params: Path<(u32, String)>) -> Result<Json<()>, Error> {
+    Ok(Json(()))
+  }
+
+  fn cfg_root(cfg: &mut ServiceConfig) {
+    cfg.configure(cfg_sub);
+  }
+
+  fn cfg_sub(cfg: &mut ServiceConfig) {
+    cfg.route("/", get().to(test));
+  }
+
+  let app = App::new()
+    .document(Spec {
+      openapi: OpenApiVersion::OAS3_0,
+      ..Default::default()
+    })
     .configure(cfg_root)
     .build("/openapi.json");
   let app = init_service(app).await;
